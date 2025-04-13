78bbf5262e8a9e9292b1e1825b4d7ccabcf0371f
dkglee <ideade9@gmail.com>
2025-04-13 17:39:44 +0900

    Feat: 네트워크 게임 프레임워크 클래스 기본 생성

---
+++ Source/JumpGame/Core/GameState/NetworkGameState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "NetworkGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JUMPGAME_API ANetworkGameState : public AGameState
+{
+	GENERATED_BODY()
+};
