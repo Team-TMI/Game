27c284a112550024de3e97c6d5ce65bfafd3b6a2
kimskyeee <104538778+kimskyeee@users.noreply.github.com>
2025-04-13 17:48:10 +0900

    Feat: 스팀 플러그인, 모듈추가, 세션기본설정 추가

---
+++ Source/JumpGame/Core/GameInstance/JumpGameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "JumpGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JUMPGAME_API UJumpGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
