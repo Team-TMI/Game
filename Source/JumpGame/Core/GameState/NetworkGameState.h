cfda7219b99d3987cacdb68a0e7b5ddf726fc9b0
dkglee <ideade9@gmail.com>
2025-04-13 18:00:19 +0900

    Feat: 네트워크 시계 추가

--- Source/JumpGame/Core/GameState/NetworkGameState.h
+++ Source/JumpGame/Core/GameState/NetworkGameState.h
@@ -13,4 +13,22 @@ UCLASS()
 class JUMPGAME_API ANetworkGameState : public AGameState
 {
 	GENERATED_BODY()
+
+public:
+	ANetworkGameState();
+
+#pragma region NetworkClock
+
+public:
+	// 서버 시계를 가져온다.
+	virtual double GetServerWorldTimeSeconds() const override;
+	
+private:
+	// 서버의 시간 (앞으로 모든 클라이언트들이 해당 시간을 동기화 할 거임)
+	UPROPERTY()
+	double ServerWorldTimeSeconds = 0.0f;
+	
+public:
+	
+#pragma endregion
 };
--- Source/JumpGame/Core/PlayerController/NetworkPlayerController.h
+++ Source/JumpGame/Core/PlayerController/NetworkPlayerController.h
@@ -13,4 +13,24 @@ UCLASS()
 class JUMPGAME_API ANetworkPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	ANetworkPlayerController();
+	
+// 네트워크 시계
+#pragma region NetworkClock
+public:
+	// 서버 시계를 가져온다.
+	float GetServerTime() const;
+	
+protected:
+	// 네트워크 시계를 만들기 위해서 필요한 함수
+	// 아래 함수에서 서버로부터 시간을 받아오도록 요청한다.
+	virtual void ReceivedPlayer() override;
+
+private:
+	UPROPERTY()
+	class UNetworkClockComponent* NetworkClockComponent = nullptr;
+#pragma endregion
+	
 };
