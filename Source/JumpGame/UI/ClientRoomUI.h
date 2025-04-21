1b6ca76cffe5f478dda70305c660ccf7afb43922
kimskyeee <104538778+kimskyeee@users.noreply.github.com>
2025-04-21 19:33:21 +0900

    Feat: Main UI 작업중

--- Source/JumpGame/UI/ClientRoomUI.h
+++ Source/JumpGame/UI/ClientRoomUI.h
@@ -37,10 +37,28 @@ public:
 	class UButton* Btn_GoFind;
 	UPROPERTY(meta = (BindWidget))
 	class UButton* Btn_GoCreateMap;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_GoFrogPass;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_GoSettings;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_GoCredit;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_GoGameEnd;
 	
 	// 눌렀을때 호출되는 함수
 	UFUNCTION()
 	void OnClickGoFindRoom();
+	UFUNCTION()
+	void OnClickGoCreateMap();
+	UFUNCTION()
+	void OnClickGoFrogPass();
+	UFUNCTION()
+	void OnClickGoSettings();
+	UFUNCTION()
+	void OnClickGoCredit();
+	UFUNCTION()
+	void OnClickGoGameEnd();
 
 	// WidgetSwitcher (1)
 	// 방 만들기 관련
