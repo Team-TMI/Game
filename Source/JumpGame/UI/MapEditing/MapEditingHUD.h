d940739c34c32c596d24a7fd5420d999ea7c7dd1
dkglee <ideade9@gmail.com>
2025-04-22 20:49:46 +0900

    Temp: 임시 저장

---
+++ Source/JumpGame/UI/MapEditing/MapEditingHUD.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MapEditingHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JUMPGAME_API UMapEditingHUD : public UUserWidget
+{
+	GENERATED_BODY()
+};
