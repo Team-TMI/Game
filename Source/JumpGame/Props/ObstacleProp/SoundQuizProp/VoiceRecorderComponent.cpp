d9b0bbaaf935ac4cbb2df85d563f31ef815b7fd9
kimskyeee <104538778+kimskyeee@users.noreply.github.com>
2025-04-24 14:30:58 +0900

    Feat: 음성 녹음 모듈 추가

---
+++ Source/JumpGame/Props/ObstacleProp/SoundQuizProp/VoiceRecorderComponent.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VoiceRecorderComponent.h"
+
+bool UVoiceRecorderComponent::Init(int32& SampleRate)
+{
+	NumChannels = 1;
+
+#if SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
+	// Initialize the DSP objects
+	Osc.Init(SampleRate);
+	Osc.SetFrequency(440.0f);
+	Osc.Start();
+#endif // SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
+
+	return true;
+}
+
+int32 UVoiceRecorderComponent::OnGenerateAudio(float* OutAudio, int32 NumSamples)
+{
+#if SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
+	// Perform DSP operations here
+	for (int32 Sample = 0; Sample < NumSamples; ++Sample)
+	{
+		OutAudio[Sample] = Osc.Generate();
+	}
+#endif // SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
+
+	return NumSamples;
+}
+
+void UVoiceRecorderComponent::SetFrequency(const float InFrequencyHz)
+{
+#if SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
+	// Use this protected base class method to push a lambda function which will safely execute in the audio render thread.
+	SynthCommand([this, InFrequencyHz]()
+	{
+		Osc.SetFrequency(InFrequencyHz);
+		Osc.Update();
+	});
+#endif // SYNTHCOMPONENT_EX_OSCILATOR_ENABLED
+}
+
+void UVoiceRecorderComponent::StartRecording()
+{
+}
+
+void UVoiceRecorderComponent::StopRecording(const bool bIsStop)
+{
+}
