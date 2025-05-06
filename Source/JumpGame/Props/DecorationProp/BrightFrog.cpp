514b516f265b93ee6e3965faab3983c6c40673cf
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-05-06 15:34:45 +0900

    Feat: Bright Frog

---
+++ Source/JumpGame/Props/DecorationProp/BrightFrog.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BrightFrog.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+ABrightFrog::ABrightFrog()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Tags.Add("Decoration");
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> TempMesh(
+		TEXT("/Game/Props/Decoration/BrightFrog/SM_BrightFrog.SM_BrightFrog"));
+	if (TempMesh.Succeeded())
+	{
+		MeshComp->SetStaticMesh(TempMesh.Object);
+	}
+	
+}
+
+// Called when the game starts or when spawned
+void ABrightFrog::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABrightFrog::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
