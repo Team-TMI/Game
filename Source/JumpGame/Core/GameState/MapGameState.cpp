c57f832a48257c490bc21cde7702b53153542037
kimskyeee <104538778+kimskyeee@users.noreply.github.com>
2025-05-08 14:08:56 +0900

    Feat: 게임스테이트 델리게이트 추가

--- Source/JumpGame/Core/GameState/MapGameState.cpp
+++ Source/JumpGame/Core/GameState/MapGameState.cpp
@@ -5,6 +5,7 @@
 
 #include "JumpGame/Core/GameInstance/JumpGameInstance.h"
 #include "JumpGame/Networks/Connection/ConnectionVerifyComponent.h"
+#include "JumpGame/UI/GameProgressBarUI.h"
 
 void AMapGameState::BeginPlay()
 {
@@ -14,4 +15,20 @@ void AMapGameState::BeginPlay()
 	// 최대 몇명의 플레이어가 플레이를 할지 설정하는 함수 (GI에서 정보를 들고와서 설정해준다)
 	int32 MaxPlayer = GI->GetMaxPlayerCount();
 	ConnectionVerifyComponent->InitMaxPlayerCount(MaxPlayer);
+}
+
+// 서버에서 호출되는 함수
+void AMapGameState::OnAllClientAdded()
+{
+	Super::OnAllClientAdded();
+
+	// 클라이언트에게 알리자 (2초후)
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AMapGameState::MulticastRPC_AllClientAdded, 2, false);
+}
+
+void AMapGameState::MulticastRPC_AllClientAdded_Implementation()
+{
+	// 브로드캐스트 하자
+	OnAllClientAddedDelegate.Broadcast();
 }
