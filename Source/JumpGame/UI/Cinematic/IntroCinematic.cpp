b9842d86c3229f06ae50eab8eefac57d69b03215
kimskyeee <zlagksnf@gmail.com>
2025-05-21 22:57:37 +0900

    Feat: 시네마틱 한번만재생, 시작시재생

---
+++ Source/JumpGame/UI/Cinematic/IntroCinematic.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IntroCinematic.h"
+
+#include "FileMediaSource.h"
+#include "MediaPlayer.h"
+
+void UIntroCinematic::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (MediaPlayer && MediaSource)
+	{
+		MediaPlayer->OpenSource(MediaSource);
+		MediaPlayer->Play();
+	}
+}
---
+++ Source/JumpGame/UI/Cinematic/IntroCinematic.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "IntroCinematic.generated.h"
+
+class UFileMediaSource;
+class UMediaPlayer;
+/**
+ * 
+ */
+UCLASS()
+class JUMPGAME_API UIntroCinematic : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnInitialized() override;
+
+	// 미디어 플레이어 (실제 재생기)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Media")
+	UMediaPlayer* MediaPlayer;
+
+	// mp4 등 외부 파일 소스
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Media")
+	UFileMediaSource* MediaSource;
+	
+};
